cd79d2ef36d661026eedbf296ecb824d02bda085
soso <sof.benzaied@gmail.com>
2019-04-11 12:22:41 +0200

    affiche pickerview

--- BDDMobile/BDDMobile/Controller/ShowItemViewController.swift
+++ BDDMobile/BDDMobile/Controller/ShowItemViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 import CoreData
 var liste=[Category]()
 
-class ShowItemViewController : UIViewController, UIPickerViewDelegate {
+class ShowItemViewController : UIViewController, UIPickerViewDelegate,UIPickerViewDataSource {
+
     
     
+    /*UIPickerViewDataSource, UIPickerViewDelegate,*/
     @IBOutlet weak var ItemPhoto: UIImageView!
     @IBOutlet weak var ItemTitle: UILabel?
     @IBOutlet weak var ItemDescription: UITextView?
@@ -25,27 +27,49 @@ class ShowItemViewController : UIViewController, UIPickerViewDelegate {
     var itemdatecreation : Date!
     var itemdatemodification : Date!
     var itemcategory : Category!
+    
+    func numberOfComponents(in ItemCategory: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ ItemCategory: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return liste.count
+    }
+    
+    func pickerView(_ ItemCategory: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        
+        itemcategory = liste[row]
+        
+    }
+    func pickerView(_ ItemCategory: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return liste[row].nomCat!
+        
+    }
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
         
         
-         
-         let formatter = DateFormatter()
-         // initially set the format based on your datepicker date / server String
-         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         
-         let myString = formatter.string(from: itemdatecreation) // string purpose I add here
+        let formatter = DateFormatter()
+        // initially set the format based on your datepicker date / server String
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        let myString = formatter.string(from: itemdatecreation) // string purpose I add here
         let myString2 = formatter.string(from: itemdatemodification)
-         // convert your string to date
-         let yourDate = formatter.date(from: myString)
+        // convert your string to date
+        let yourDate = formatter.date(from: myString)
         let yourDate2 = formatter.date(from: myString2)
-         //then again set the date format whhich type of output you need
-         formatter.dateFormat = "dd-MMM-yyyy HH:mm:ss"
-         // again convert your date to string
-         let myStringafd = formatter.string(from: yourDate!)
-         let myStringafd2 = formatter.string(from: yourDate2!)
+        //then again set the date format whhich type of output you need
+        formatter.dateFormat = "dd-MMM-yyyy HH:mm:ss"
+        // again convert your date to string
+        let myStringafd = formatter.string(from: yourDate!)
+        let myStringafd2 = formatter.string(from: yourDate2!)
         
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -65,7 +89,7 @@ class ShowItemViewController : UIViewController, UIPickerViewDelegate {
             let results = try context.fetch(fetchRequest)
             
             liste = results
-       
+            ItemCategory.reloadComponent(0)
             
             if results.count > 0{
                 for r in results as! [NSManagedObject]{
@@ -79,28 +103,17 @@ class ShowItemViewController : UIViewController, UIPickerViewDelegate {
         { print("Could not fetch : \(error)")
         }
         
-         
-         
-  
+        
+        
+        
         ItemTitle?.text=texteTitre
         ItemDescription?.text=texteTitre
         ItemPhoto.image =  itemPhoto
         ItemDateCreate.text = myStringafd
         ItemDateModif.text  = myStringafd2
-    }
-    
-    
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
-    {
-        return liste.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
-    {
-        
-        itemcategory = liste[row]
+        ItemCategory.reloadComponent(0)
         
     }
+    
     
 }
